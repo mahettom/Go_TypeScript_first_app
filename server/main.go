8e8f3b1f792147d435a526785d4b9ca13d0aacbe
TomMahe <mahettom@gmail.com>
2023-07-11 18:54:56 +0200

    create routes | test ok

--- server/main.go
+++ server/main.go
@@ -66,8 +66,14 @@ func main() {
 				break
 			}
 		}
-
 		return c.JSON((todos))
+	})
+
+	// ————————————————————————————————————————————————————————————————————————————————————————
+	// ———————————————————————————————————————————————————————————————————————————— PATCH ROUTE
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+
+		return c.JSON(todos)
 	})
 
 	log.Fatal(app.Listen(":4000"))
